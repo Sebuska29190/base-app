6f7350873cba912847f22c18820ad7a40b5e4e40
Buska <57123390+Sebuska29190@users.noreply.github.com>
2026-01-22 17:51:56 +0100

    Update WalletConnect projectId

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -5,7 +5,7 @@ import { walletConnect } from 'wagmi/connectors'
 export const config = createConfig({
   chains: [base],
   connectors: [
-    walletConnect({ projectId: 'demo-project-id' }),
+    walletConnect({ projectId: 'ee357177a8f40eb47b61ea2eccc5ba46' }),
   ],
   transports: {
     [base.id]: http(),
